c5d87c7f6a82d0661cb8922a342bf618ffd1a13a
Saidouchrif <166962901+Saidouchrif@users.noreply.github.com>
2026-03-02 13:53:41 +0100

    version de code avec test de db connexion

--- Code/Backend/services/auth-service/src/app.js
+++ Code/Backend/services/auth-service/src/app.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const pool = require('./config/db');
+
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
@@ -11,7 +14,19 @@ app.get('/health', (req, res) => {
   res.json({ status: 'Auth Service Running' });
 });
 
-const PORT = process.env.PORT || 5001;
+//test de connexion db
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({
+      message: 'Database Connected',
+      time: result.rows[0]
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Database error' });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Auth Service running on port ${PORT}`);
 });
