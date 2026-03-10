678d5e5b074ee465a3565bc2405624b347faf7e1
Saidouchrif <166962901+Saidouchrif@users.noreply.github.com>
2026-03-10 13:45:28 +0100

    version de code avec l auth avec jwt

--- Code/Backend/services/auth-service/src/app.js
+++ Code/Backend/services/auth-service/src/app.js
@@ -1,30 +1,35 @@
 require('dotenv').config();
+require('./Models');
+
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/db');
-require('./Models');
 const app = express();
+const routes = require('./Routes');
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api', routes);
+
 
 app.get('/health', (req, res) => {
   res.json({ status: 'Auth Service Running' });
 });
 
-const PORT = process.env.PORT || 5001;
 
 sequelize.authenticate()
   .then(() => {
-    console.log('✅ Connected to PostgreSQL');
+    console.log('Connected to PostgreSQL');
     return sequelize.sync();
   })
   .then(() => {
-    console.log('✅ Tables synced');
+    console.log('Tables synced');
     app.listen(PORT, () => {
       console.log(`Auth Service running on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.error('❌ DB Error:', err);
+    console.error('DB Error:', err);
   });
