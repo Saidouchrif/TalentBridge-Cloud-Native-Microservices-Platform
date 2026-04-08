a75cb18f29a1ab7e82ff89146d6f4298ad74943b
ARCHILLE7 <126948300+ARCHILLE7@users.noreply.github.com>
2026-04-08 01:20:43 +0100

    Correction Workflow entreprise-service Version 3.0

--- Code/Backend/services/entreprise-service/jest.config.js
+++ Code/Backend/services/entreprise-service/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   // On cible les tests dans `tests/`
   testMatch: ["**/tests/**/*.test.js"],
   // On utilise le pattern communjs pour rester cohérent avec le reste du repo
-  testEnvironment: "node"
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.js"]
 };
 
